568212a033f39f57c13406a813a8d77d4fa55b71
Larry Schirmer <larryschirmer@gmail.com>
2018-09-25 23:00:01 -0400

    add schema config file for constructing db url

---
+++ database/models/config.js
@@ -0,0 +1,7 @@
+const { db_host, db_port } = require('../config');
+
+const DBHOST = db_host;
+const DBPORT = db_port;
+const DBURL = db => `mongodb://${DBHOST}:${DBPORT}/${db}`;
+
+module.exports = { DBURL };
